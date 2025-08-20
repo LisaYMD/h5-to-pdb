97c808d1888fab6f937562f370eefe57b6e12b8c
LisaYMD <phi.1618.bismuth83@gmail.com>
2025-08-20 15:31:10 +0900

    save file after reading

--- h5toPDB/h5_parser_pdb.py
+++ h5toPDB/h5_parser_pdb.py
@@ -16,10 +16,14 @@ class h5toPDB:
         self.molnum = [types[k][1] for k in range(0, len(types))]
         self.mol_diffusion = [types[k][2] for k in range(0, len(types))]
         # read trajectory
+        if 'trajectory' not in f['readdy']:
+            raise ValueError("No trajectory found in the provided h5 file.")
         self.limrec = f['readdy']['trajectory']['limits']
         self.rec = f['readdy']['trajectory']['records']
         self.traj_len = int(self.limrec.shape[0]-1)
         # read topologies
+        if 'topologies' not in f['readdy']['observables']:
+            raise ValueError("No topologies found in the provided h5 file.")
         topo = f['readdy']['observables']['topologies']
         self.limparts = topo['limitsParticles']
         self.limedges = topo['limitsEdges']
@@ -96,43 +100,48 @@ class h5toPDB:
         return resid_candidates, molcompose, molsize, molcolor
     
     def generate_pdb(self):
+        print("Generating PDB file...")
         start, duration = 0, self.traj_len
         edge_data = self.connected_edge(0)
         self.moltype, self.molnum, self.molsize, self.molcolor = self.update_existingtypes()
         resid_candidates, molcompose, _, _ = self.resid_from_setting()
         fname = self.filename[:-3] + ".pdb"
-        f = open(fname, "w")
-        f.write("CRYST1"+"  "+f"{format(self.xbox*10, '.2f'):<8}"+" "+f"{format(self.ybox*10, '.2f'):<8}"+" "+f"{format(self.zbox*10, '.2f'):<8}"+" 90.00  90.00  90.00"+"\n")
+        # record the writing strings in list "lines"
         resid = "PSD" # by default
         ### Generate PDB file
-        for t in tqdm(range(start, start+duration)):
-            f.write("MODEL\n")
-            for m in range(self.limrec[t,0], self.limrec[t,1]):
-                pos0 = str(format(10*self.rec[m][3][0], '.1f')) # 166.2
-                pos1 = str(format(10*self.rec[m][3][1], '.1f')) # 218.1
-                pos2 = str(format(10*self.rec[m][3][2], '.1f')) # -412.2
-                position = " "*(7-len(pos0)+4)+ pos0 + " "*(7-len(pos1)+1) + pos1 + " "*(7-len(pos2)+1) + pos2
-                if self.rec[m][1] < 10:
-                    space = "   "
-                elif self.rec[m][1] < 100:
-                    space = "  "
-                elif self.rec[m][1] < 1000:
-                    space = " "
-                else:
-                    space = ""
-                if resid_candidates != []:
-                    for r in range(0, len(resid_candidates)):
-                        if self.moltype[self.molnum.index(self.rec[m][0])] in molcompose[resid_candidates[r]]:
-                            resid = resid_candidates[r][:3]
-                            break
-                f.write("ATOM   "+space+str(self.rec[m][1])+" "+f"{self.moltype[self.molnum.index(self.rec[m][0])][:4]:<4}"+" "+resid+" A"+space+str(self.rec[m][1])+position+"  0.00  0.00\n")
-            f.write("ENDMDL\n")
-        f.write("END") 
-        f.close()
+        with open(fname, "w") as f:
+            f.write("CRYST1  "+f"{format(self.xbox*10, '.2f'):<8}"+" "+f"{format(self.ybox*10, '.2f'):<8}"+" "+f"{format(self.zbox*10, '.2f'):<8}"+" 90.00  90.00  90.00\n") 
+            for t in tqdm(range(start, start+duration)):
+                lines = []
+                lines.append("MODEL\n")
+                for m in range(self.limrec[t,0], self.limrec[t,1]):
+                    pos0 = str(format(10*self.rec[m][3][0], '.1f')) 
+                    pos1 = str(format(10*self.rec[m][3][1], '.1f')) 
+                    pos2 = str(format(10*self.rec[m][3][2], '.1f')) 
+                    position = " "*(7-len(pos0)+4)+ pos0 + " "*(7-len(pos1)+1) + pos1 + " "*(7-len(pos2)+1) + pos2
+                    if self.rec[m][1] < 10:
+                        space = "   "
+                    elif self.rec[m][1] < 100:
+                        space = "  "
+                    elif self.rec[m][1] < 1000:
+                        space = " "
+                    else:
+                        space = ""
+                    if resid_candidates != []:
+                        for r in range(0, len(resid_candidates)):
+                            if self.moltype[self.molnum.index(self.rec[m][0])] in molcompose[resid_candidates[r]]:
+                                resid = resid_candidates[r][:3]
+                                break
+                    lines.append("ATOM   "+space+str(self.rec[m][1])+" "+f"{self.moltype[self.molnum.index(self.rec[m][0])][:4]:<4}"+" "+resid+" A"+space+str(self.rec[m][1])+position+"  0.00  0.00\n")
+                lines.append("ENDMDL\n")
+                f.write("".join(lines))
+            f.write("END")
+        print("PDB file generated: " + fname)
         return None
 
     ### necessary for drawing edge
     def generate_tcl(self):
+        print("Generating Tcl script file...")
         resid_candidates, molcompose, _, _ = self.resid_from_setting()
         self.moltype, self.molnum, self.molsize, self.molcolor = self.update_existingtypes() 
         edge_list = []
@@ -142,69 +151,75 @@ class h5toPDB:
         ### Generate Tcl script file
         fname_tcl = self.filename[:-3] + ".pdb.tcl"
         with open(fname_tcl, 'w') as g:
-            g.write("mol delete top\n")
-            g.write("mol load pdb "+fname_tcl[:-4]+"\n")
-            g.write("mol delrep 0 top\n")
-            g.write("display resetview\n")
+            lines_tcl = []
+            lines_tcl.append("mol delete top\n")
+            lines_tcl.append("mol load pdb "+fname_tcl[:-4]+"\n")
+            lines_tcl.append("mol delrep 0 top\n")
+            lines_tcl.append("display resetview\n")
             overlap = np.zeros(len(self.moltype))
             for l in range(0, len(self.moltype)):
-                g.write("mol representation VDW "+str(self.molsize[l]*6.6)+"\n")
-                g.write("mol selection name "+self.moltype[l]+"\n")
+                lines_tcl.append("mol representation VDW "+str(self.molsize[l]*6.6)+"\n")
+                lines_tcl.append("mol selection name "+self.moltype[l]+"\n")
                 if resid_candidates != []:
                     for r in range(0, len(resid_candidates)):
                         if self.moltype[l] in molcompose[resid_candidates[r]]:
-                            g.write("mol color ColorID "+str(int(self.molcolor[l]))+"\n")
+                            lines_tcl.append("mol color ColorID "+str(int(self.molcolor[l]))+"\n")
                             break
                 else:
-                    g.write("mol color ColorID "+str(int(self.molcolor[l]))+"\n")
-                g.write("mol addrep top\n")
-            g.write("animate goto 0\n")
-            g.write("color Display Background white\n")
-            g.write("molinfo top set {center_matrix} {{{1 0 0 0}{0 1 0 0}{0 0 1 0}{0 0 0 1}}}\n")
-            g.write("set x "+str(self.xbox*10)+"\n")
-            g.write("set y "+str(self.ybox*10)+"\n")
-            g.write("set z "+str(self.zbox*10)+"\n")
+                    lines_tcl.append("mol color ColorID "+str(int(self.molcolor[l]))+"\n")
+                lines_tcl.append("mol addrep top\n")
+            lines_tcl.append("animate goto 0\n")
+            lines_tcl.append("color Display Background white\n")
+            lines_tcl.append("molinfo top set {center_matrix} {{{1 0 0 0}{0 1 0 0}{0 0 1 0}{0 0 0 1}}}\n")
+            lines_tcl.append("set x "+str(self.xbox*10)+"\n")
+            lines_tcl.append("set y "+str(self.ybox*10)+"\n")
+            lines_tcl.append("set z "+str(self.zbox*10)+"\n")
             # draw lines between each connected domains
             # adding bonds
-            g.write("set sel [atomselect $top all]\n")
+            lines_tcl.append("set sel [atomselect $top all]\n")
             for e in range(0, len(edge_list)):
-                g.write("topo addbond "+str(edge_list[e][1])+" "+str(edge_list[e][0])+"\n")
+                lines_tcl.append("topo addbond "+str(edge_list[e][1])+" "+str(edge_list[e][0])+"\n")
             # drawing bonds
-            g.write("mol representation Bonds 1.5\n")
+            lines_tcl.append("mol representation Bonds 1.5\n")
             if resid_candidates != []:
                 for r in range(0, len(resid_candidates)):
-                    g.write("mol selection resname "+str(resid_candidates[r][:3])+"\n")
-                    g.write("mol color ColorID "+str(int(r+1))+"\n")
-                    g.write("mol addrep top\n")
+                    lines_tcl.append("mol selection resname "+str(resid_candidates[r][:3])+"\n")
+                    lines_tcl.append("mol color ColorID "+str(int(r+1))+"\n")
+                    lines_tcl.append("mol addrep top\n")
             else:
-                g.write("mol selection resname PSD\n")
-                g.write("mol color ColorID 2\n")
-                g.write("mol addrep top\n")
+                lines_tcl.append("mol selection resname PSD\n")
+                lines_tcl.append("mol color ColorID 2\n")
+                lines_tcl.append("mol addrep top\n")
             # drawing pbc box
-            g.write("pbc set {$x $y $z 90.0 90.0 90.0 -all}\n")
-            g.write("pbc box_draw -center origin -color black\n")
+            lines_tcl.append("pbc set {$x $y $z 90.0 90.0 90.0 -all}\n")
+            lines_tcl.append("pbc box_draw -center origin -color black\n")
             # if necesssary: wrap bonds
+            g.write("".join(lines_tcl))
+        print("Tcl script file generated: " + fname_tcl)
         return None
     
     ## generate lammps data file
     def generate_lammpstrj(self):
+        print("Generating LAMMPS trajectory file...")
         start, duration = 0, self.traj_len
         edge_data = self.connected_edge(0)
         self.moltype, self.molnum, self.molsize, self.molcolor = self.update_existingtypes()
         resid_candidates, molcompose, self.molsize, self.molcolor = self.resid_from_setting()
         fname = self.filename[:-3] + ".lammpstrj"
-        f = open(fname, "w")
-        for t in tqdm(range(start, start+duration)):
-            f.write("ITEM: TIMESTEP\n"+str(t)+"\nITEM: NUMBER OF ATOMS\n"+str(self.limrec[t,1]-self.limrec[t,0])+"\nITEM: BOX BOUNDS pp pp pp\n")
-            f.write(str(-self.xbox/2)+" "+str(self.xbox/2)+"\n"+str(-self.ybox/2)+" "+str(self.ybox/2)+"\n"+str(-self.zbox/2)+" "+str(self.zbox/2)+"\n")
-            f.write("ITEM: ATOMS id type xs ys zs\n")
-            for m in range(self.limrec[t,0], self.limrec[t,1]):
-                pos0 = str(format(self.rec[m][3][0], '.6f'))
-                pos1 = str(format(self.rec[m][3][1], '.6f'))
-                pos2 = str(format(self.rec[m][3][2], '.6f'))
-                resid = 1
-                f.write(str(self.rec[m][1])+" "+str(resid)+" "+pos0+" "+pos1+" "+pos2+"\n")
-        f.close()
+        with open(fname, "w") as f:
+            for t in tqdm(range(start, start+duration)):
+                lines = []
+                lines.append("ITEM: TIMESTEP\n"+str(t)+"\nITEM: NUMBER OF ATOMS\n"+str(self.limrec[t,1]-self.limrec[t,0])+"\nITEM: BOX BOUNDS pp pp pp\n")
+                lines.append(str(-self.xbox/2)+" "+str(self.xbox/2)+"\n"+str(-self.ybox/2)+" "+str(self.ybox/2)+"\n"+str(-self.zbox/2)+" "+str(self.zbox/2)+"\n")
+                lines.append("ITEM: ATOMS id type xs ys zs\n")
+                for m in range(self.limrec[t,0], self.limrec[t,1]):
+                    pos0 = str(format(self.rec[m][3][0], '.6f'))
+                    pos1 = str(format(self.rec[m][3][1], '.6f'))
+                    pos2 = str(format(self.rec[m][3][2], '.6f'))
+                    resid = 1
+                    lines.append(str(self.rec[m][1])+" "+str(resid)+" "+pos0+" "+pos1+" "+pos2+"\n")
+                f.write("".join(lines))
+        print("LAMMPS trajectory file generated: " + fname)
         return None
 
 if __name__ == '__main__':
